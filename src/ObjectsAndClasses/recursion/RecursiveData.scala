ebc5853d9cd2b5f1007872a094c96b564328e0f0
AndrewOdiit <andrewodiit2020@gmail.com>
2021-02-25 19:32:36 +0300

    [learning-scala] structural recursion polymorphism & pattern matching

--- src/ObjectsAndClasses/recursion/RecursiveData.scala
+++ src/ObjectsAndClasses/recursion/RecursiveData.scala
@@ -27,9 +27,53 @@ object RecursiveData extends App {
     }
   }
   case object End extends IntList
-  final case class Pair(head:Int, tail:IntList) extends IntList{
+  final case class Pair(head:Int, tail:IntList) extends IntList
+
+
+  //structural recursion using pattern matching.
+  sealed trait RecursiveTree {
+    def sum(): Int ={
+      this match {
+        case Leaf(value) => value
+        case Node(left, right) => left.sum + right.sum
+      }
+    }
+
+    def double(): RecursiveTree ={
+      this match {
+        case Leaf(value) => Leaf(value*2)
+        case Node(left, right) => Node(left.double, right.double)
+      }
+    }
+
 
   }
+
+  case class Node(left:RecursiveTree, right:RecursiveTree) extends RecursiveTree {
+
+  }
+
+  case class Leaf(value:Int) extends RecursiveTree {
+
+  }
+
+  sealed trait Btree{
+    def sum(): Int
+    def double(): Btree
+  }
+
+  final case class PNode(l:Btree, r: Btree) extends Btree{
+    override def sum(): Int = l.sum + r.sum
+
+    override def double(): Btree = PNode(l.double(), r.double())
+  }
+
+  final case class Pleaf(value:Int) extends  Btree{
+    override def sum(): Int = value
+
+    override def double(): Btree = Pleaf(value * 2)
+  }
+
 
 
 
@@ -43,5 +87,18 @@ object RecursiveData extends App {
   assert(example.product == 6)
   assert(example.tail.product == 6)
   assert(End.product == 1)
+//  val bTree = Node(Leaf(1), Leaf(2))
+//  val bTree2 = Node(Leaf(4), Leaf(5))
+  val PTree = PNode(Pleaf(1), Pleaf(2))
+  val PTree2 = PNode(Pleaf(4), Pleaf(5))
+  val PTree3 = PNode(PNode(Pleaf(10), Pleaf(10)), PNode(Pleaf(10), Pleaf(10))) //49 + 64 --
+//  assert(bTree.double() == Node(Leaf(2), Leaf(4)))
+//  assert(bTree2.double() == Node(Leaf(8), Leaf(10)))
+  assert(PTree.double() == PNode(Pleaf(2), Pleaf(4)))
+  assert(PTree2.double() == PNode(Pleaf(8), Pleaf(10)))
+  assert(PTree3.double() == PNode(PNode(Pleaf(20), Pleaf(20)), PNode(Pleaf(20), Pleaf(20))))
+
+
   println("All tests passed...")
+
 }
