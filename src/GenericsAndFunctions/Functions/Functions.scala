c91f5816e97f1cc6e152e7b24d6a154b434ee144
AndrewOdiit <andrewodiit2020@gmail.com>
2021-03-02 19:02:39 +0300

    [learning-scala] keep going

--- src/GenericsAndFunctions/Functions/Functions.scala
+++ src/GenericsAndFunctions/Functions/Functions.scala
@@ -2,30 +2,19 @@ package GenericsAndFunctions.Functions
 
 object Functions extends App {
   sealed trait IntList {
-    def length: Int =
-      fold(0, (_, tl) => 1 + tl)
-    def double(): IntList = ???
-//      this match {
-//        case End => End
-//        case Pair(hd, tl) => Pair(hd * 2, tl.double)
-//      }
-    def product: Int =
-      fold(1, (hd, tl) => hd * tl)
-    def sum: Int =
-      fold(0, (hd, tl) => (hd + tl))
+    def length: Int =  fold[Int](0, (_, tl) => 1 + tl)
+    def double: IntList = {
+      fold[IntList](End,(hd, tl) => Pair(hd * 2, tl))
+    }
 
-    def fold(n:Int, fn:(Int, Int)=>Int): Int ={
+    def product: Int =  fold[Int](1, (hd, tl) => hd * tl)
+    def sum: Int = fold[Int](0, (hd, tl) => (hd + tl))
+
+    def fold[A](n: A, fn:(Int, A)=>A): A ={
       this match {
         case End =>n
         case Pair(hd, tl) => fn(hd, tl.fold(n, fn))
       }
-    }
-
-    def fold(n:Int, fn:(Int, IntList)=> IntList): IntList ={
-        this match {
-          case End => End
-          case Pair(hd, tl) => fn(hd * n, tl.fold(n, fn))
-        }
     }
   }
 
@@ -37,6 +26,6 @@ object Functions extends App {
   val example = Pair(1, Pair(2, Pair(3, End)))
   assert(example.length == 3)
   assert(example.product == 6)
-  //assert(example.double() == Pair(2, Pair(4, Pair(6, End))))
+  assert(example.double == Pair(2, Pair(4, Pair(6, End))))
   println("Skeleton is fine...")
 }
