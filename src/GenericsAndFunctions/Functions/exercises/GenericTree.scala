0ee1d21b93824b674fac210ba29db55f44f36fb2
AndrewOdiit <andrewodiit2020@gmail.com>
2021-03-06 09:55:30 +0300

    [learning-scala] Generic Folds (Keep going)

--- src/GenericsAndFunctions/Functions/exercises/GenericTree.scala
+++ src/GenericsAndFunctions/Functions/exercises/GenericTree.scala
@@ -2,25 +2,37 @@ package GenericsAndFunctions.Functions.exercises
 
 object GenericTree extends App{
   sealed trait BTree[A] {
-    def double():BTree[Int] ={
-      this match {
-        case Leaf(value:Int) => Leaf(value * 2)
-        case Node(left, right) => Node(left.double(), right.double())
-        case _ => throw new Exception("Something went wrong")
-      }
-
-    }
-
-//    def fold_and_double():BTree[Int]{
-//       fold
+     //Make this work for both strings & integers, double should double whatever it find's there
+//    def double():BTree[Int] ={
+//      this match {
+//        case Leaf(value:Int) => Leaf(value * 2)
+//        case Node(left, right) => Node(left.double(), right.double())
+//        case _ => throw new Exception("Something went wrong")
+//      }
+//
 //    }
-    //review this in the morning...try and use or adapt for double method.
-    def fold[B](leaf: A => B)(node:(B, B)=>B):B ={ //not too bad , just some issues with types leaf takes A (values) --> B leaf(value)
+
+
+    /*
+    override def toString: String = {
+      //this demonstrates how fold would be used, but doesn't work.
+      fold[String]((value:String)=>value, (left, right) => left + " " +  right)
+    }
+     */
+
+    def printElements(): String ={
+          this match {
+            case Leaf(value) => value.toString
+            case Node(left, right) => left.printElements() + " " + right.printElements()
+          }
+    }
+    def fold[B](leaf: A => B)(node:(B, B)=>B):B ={
         this match {
           case Leaf(value) => leaf(value)
           case Node(left, right) => node(left.fold(leaf){node}, right.fold(leaf){node})
         }
     }
+
 
 
   }
@@ -29,7 +41,11 @@ object GenericTree extends App{
   final case class Leaf[A](value:A) extends BTree[A]
 
 
-  val example = Node
+  val tree = Node(Node(Leaf("To"), Leaf("iterate")),
+    Node(Node(Leaf("is"), Leaf("human,")),
+      Node(Leaf("to"), Node(Leaf("recurse"), Leaf("divine")))))
+
+  println(tree.printElements())
 
 
 }
